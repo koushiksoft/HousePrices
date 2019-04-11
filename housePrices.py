07502026daae5850a2e43f2d6cf9b83452e68fb6
koushik <koushiksoft@gmail.com>
2019-04-11 11:47:37 +0530

    First project created

---
+++ housePrices.py
@@ -0,0 +1,17 @@
+import tensorflow as tf
+import numpy as np
+from tensorflow import keras
+
+model = tf.keras.Sequential([keras.layers.Dense(units=1, input_shape=[1])])
+model.compile(optimizer='sgd', loss='mean_squared_error')
+xs = np.array([1.0, 2.0, 3.0, 4.0, 5.0, 6.0, 7.0, 8.0, 9.0, 10.0])
+ys = np.array([1.0, 1.5, 2.0, 2.5, 3.0, 3.5, 4.0, 4.5, 5.0, 5.5])
+model.fit(xs, ys, epochs=5000)
+print(model.predict([21.0]))
+
+# 11.0  > 6.028263   - 500   - 10
+# 11.0  > 6.0056796  - 1000  - 10
+# 11.0  > 6.000003   - 5000  - 10 <<
+# 11.0  > 6.000003   - 10000 - 10
+# 21.0  > 11.000009  - 5000  - 10 <<
+# 21.0  > 11.000009  - 10000 - 10
